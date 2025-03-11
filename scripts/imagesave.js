8d5a77ad0218065c1c92d958348c637ce378be51
Stevie Lapre <stevielapre@gmail.com>
2025-03-11 22:27:26 +0100

    finalized journey and set-up about

--- scripts/imagesave.js
+++ scripts/imagesave.js
@@ -1,32 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const modal = document.getElementById('imageModal');
-    const modalImg = document.getElementById('modalImage');
-    const closeBtn = document.getElementsByClassName('close')[0];
-
-    const images = document.querySelectorAll('img');
-    images.forEach(img => {
-        img.onclick = function() {
-            modal.style.display = 'block';
-            modalImg.src = this.src;
-        }
-
-        img.addEventListener('contextmenu', function(e) {
-            e.preventDefault();
-        });
-    });
-
-    closeBtn.onclick = function() {
-        modal.style.display = 'none';
-    }
-
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = 'none';
-        }
-    }
-
-    // Disable right-click context menu on the entire document
+// Disable right-click context menu on the entire document
     document.addEventListener('contextmenu', function(e) {
         e.preventDefault();
     });
-});
+    

f6b6bdbcf1e0a674d59a66ad36f618f18a6ce3fa
Stevie Lapre <stevielapre@gmail.com>
2025-04-01 21:40:25 +0200

    completed contacted page, installed mail traffic, and started routing for dynamicy...rememeber to test the full thankyou.html when going to prod

---
+++ route/route.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const routes = {
+        '/': '/index.html',
+        '/journey': '/pages/journey.html',
+        '/about': '/pages/about.html',
+        '/contact': '/pages/contact.html',
+        '/courses': '/pages/course.html',
+        '/thankyou': '/pages/thankyou.html',
+        };
+    });
